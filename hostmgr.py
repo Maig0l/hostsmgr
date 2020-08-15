5a68698a0e70931a233e12e9d2f19d5780399cd7
Miguel <maigol@tuta.io>
2020-08-14 23:14:19 -0300

    Added basic group addition and removal. Also, #comments!

--- hostmgr.py
+++ hostmgr.py
@@ -2,11 +2,14 @@
 
 import simplejson as json
 
-groups = json.loads(open("hostgrps.json").read())
+GROUPS_FILE = "groups.json"
+groups = json.loads(
+                open(GROUPS_FILE, "r").read())
 
 def hostFormat(host):
     return f"127.0.0.1\t{host}\n"
 
+# Returns entire new hosts file (string)
 def makeFile():
     result = hostFormat("localhost") + "\n"
 
@@ -20,4 +23,29 @@ def makeFile():
             print(f"Enabled {name}")
 
     return result
+
+# Writes changes to groups file
+def updateGroupsFile():
+    open(GROUPS_FILE, "w").write(
+                            json.dumps(groups))
+
+# Appends all lines from a file to a group
+#  and saves to groups.json
+def addFromFile(group, filename):
+    # If group doesn't exist, create and enable it
+    if group not in groups:
+        groups[group] = [True]
+
+    with open(filename, "r") as newHosts:
+        for line in newHosts:
+            groups[group].append(line)
+
+    updateGroupsFile()
+
+def removeGroup(group):
+    try:
+        groups.pop(group)
+        updateGroupsFile()
+    except KeyError:
+        exit(f" /!\ Group {group} doesn't exit.")
 
